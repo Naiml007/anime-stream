549907aed3ccce444fba4ba3ad0dfde888dfcfbf
Kate <katej61487@gmail.com>
2023-02-15 16:21:07 -0500

    Initial React App set up, added requirements.txt

---
+++ ghi/src/App.jsx
@@ -0,0 +1,13 @@
+import "./Styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      HELLO THERE
+      </header>
+    </div>
+  );
+};
+
+export default App;
--- routers/anime_routes.py
+++ routers/anime_routes.py
@@ -1,27 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 import requests
 
 router = APIRouter()
-
+BASE_URL = "https://gogoanime.consumet.stream"
 
 @router.get("/recent_release")
 async def recent_release():
-    url = "https://gogoanime.consumet.stream/recent-release"
+    url = f"{BASE_URL}/recent-release"
     response = requests.get(url, params={"page": 1, "type": 1})
     data = response.json()
-    return data
+    if data:
+        return data
+    else:
+        raise HTTPException(400, "Something went wrong.")
 
 @router.get("/search")
 async def search_anime(keyword: str):
-    url = "https://gogoanime.consumet.stream/search"
+    url = f"{BASE_URL}/search"
     params = {"keyw": keyword}
     response = requests.get(url, params=params)
     data = response.json()
-    return data
+    if data: 
+        return data
+    else:
+        raise HTTPException(404, detail="Keyword not found")
 
 @router.get("/details/{animeId}")
 async def get_anime_details(animeId:str):
-    url = f'https://gogoanime.consumet.stream/anime-details/{animeId}'
-    response = requests.get(url)
-    data = response.json()
-    return data
+    try:
+        url = f'{BASE_URL}/anime-details/{animeId}'
+        response = requests.get(url)
+        data = response.json()
+        return data
+    except:
+        raise HTTPException(400, detail="Bad Request")
